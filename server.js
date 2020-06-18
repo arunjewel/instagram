d81d29f5c6347355faeb8342f67c133d0f6af763
arunjewel <arunjewel@gmail.com>
2020-06-18 13:52:00 +0530

    my first commit

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 //Install express server
 const express = require('express');
 const path = require('path');
- 
 const app = express();
- 
-// Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/angularapp'));
- 
-app.get('/*', function(req,res) {
- 
-res.sendFile(path.join(__dirname+'/angularapp/index.html'));
+
+// Serve static files....
+app.use(express.static(__dirname + '/dist/insta'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/insta/index.html'));
 });
- 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
