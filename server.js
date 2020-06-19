d3a0924adc286b8d66ebce41ccd9846f764081be
arunjewel <arunjewel@gmail.com>
2020-06-19 15:11:18 +0530

    nag

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ var serveStatic = require('serve-static');
 
 var app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+var distDir = __dirname + "/dist/";
+ app.use(express.static(distDir));
 
 var port = process.env.PORT || 8000
 app.listen(port);
