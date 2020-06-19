9ce3d2eaa8f8884ac52a42e38488e34f3da3732f
arunjewel <arunjewel@gmail.com>
2020-06-19 14:57:41 +0530

    nit

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 //Install express server
-const express = require('express');
-const path = require('path');
-const app = express();
 
-// Serve static files....
-app.use(express.static(__dirname + '/dist/insta'));
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/insta/index.html'));
-});
+
+
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
+
+var app = express()
+app.use(serveStatic(path.join(__dirname, 'dist')))
+
+var port = process.env.PORT || 8000
+app.listen(port)
+console.log('server started ' + port)
